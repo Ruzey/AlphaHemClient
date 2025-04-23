5058c731b679c2ae35d640acb0407f1b1cf0a89a
Ruzey <mattias.grenholm@gmail.com>
2025-04-23 13:14:58 +0200

    RealtorList.razor är exempelkod för att hämta data ur endpoint.

---
+++ AlphaHemClient/Pages/RealtorList.razor.cs
@@ -0,0 +1,23 @@
+﻿using AlphaHemAPI.Data.DTO;
+using System.Net.Http.Json;
+
+namespace AlphaHemClient.Pages
+{
+    public partial class RealtorList
+    {
+        private List<RealtorDto> realtors;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var response = await Http.GetFromJsonAsync<List<RealtorDto>>("https://localhost:7109/api/Realtor");
+            if (response != null)
+            {
+                realtors = response;
+            }
+            else
+            {
+                realtors = new List<RealtorDto>();
+            }
+        }
+    }
+}
