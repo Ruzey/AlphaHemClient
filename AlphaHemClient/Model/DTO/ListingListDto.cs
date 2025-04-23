2ba3d5a942a49a4fb57ae07451d6bea55fda18c5
Ruzey <mattias.grenholm@gmail.com>
2025-04-23 12:53:05 +0200

    Add project files.

---
+++ AlphaHemClient/Model/DTO/ListingListDto.cs
@@ -0,0 +1,13 @@
+﻿namespace AlphaHemAPI.Data.DTO
+{
+    // Author : Smilla
+    public class ListingListDto
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Municipality { get; set; }
+        public string RealtorFullName { get; set; }
+    }
+}
