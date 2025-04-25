ec042c49899a5b9044bbc2c4514d0688e5fc3fc1
Christoffer Snobl <csnobl@hotmail.com>
2025-04-25 10:43:20 +0200

    la till listingprofile för automapper

---
+++ AlphaHemClient/Mappings/ListingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using AlphaHemClient.Model.DTO;
+using AlphaHemClient.Model.ViewModel;
+
+namespace AlphaHemClient.Mappings
+{
+    // Author: Christoffer
+    public class ListingProfile : Profile
+    {
+        public ListingProfile()
+        {
+            CreateMap<PagedListingListDto, ListingPageViewModel>();
+        }
+    }
+}
