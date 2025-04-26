bd358772571bc4c6a95e828a560b44a23f47601c
Christoffer Snobl <csnobl@hotmail.com>
2025-04-26 18:31:42 +0200

    Konfiugrerade mappningen mellan kommundto och kommunvm + småfix

---
+++ AlphaHemClient/Mappings/MunicipalityProfile.cs
@@ -0,0 +1,15 @@
+﻿using AlphaHemClient.Model.ViewModel;
+using AlphaHemClient.Model.DTO;
+using AutoMapper;
+
+namespace AlphaHemClient.Mappings
+{
+    // Author: Christoffer
+    public class MunicipalityProfile : Profile
+    {
+        public MunicipalityProfile()
+        { 
+            CreateMap<MunicipalityListDto, MunicipalityViewModel>();
+        }
+    }
+}
--- AlphaHemClient/Model/ViewModel/MunicipalityViewModel.cs
+++ AlphaHemClient/Model/ViewModel/MunicipalityViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace AlphaHemClient.Model.ViewModel
 {
+    // Author: Christoffer
     public class MunicipalityViewModel
     {
         public string Name { get; set; } = string.Empty;
