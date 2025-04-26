b52f942c22d85ecce552cfe89e43811ed745dc5a
Christoffer Snobl <csnobl@hotmail.com>
2025-04-26 17:56:10 +0200

    Municipalityselector komponenten är klar

--- AlphaHemClient/Components/MunicipalitySelector.razor.cs
+++ AlphaHemClient/Components/MunicipalitySelector.razor.cs
@@ -1,7 +1,27 @@
-﻿namespace AlphaHemClient.Components
+﻿using Microsoft.AspNetCore.Components;
+using AlphaHemClient.Model.ViewModel;
+
+namespace AlphaHemClient.Components
 {
+    // Author: Christoffer
     public partial class MunicipalitySelector
     {
+        [Parameter] public List<MunicipalityViewModel> Municipalities { get; set; } = new List<MunicipalityViewModel>();
+        [Parameter] public EventCallback<string?> OnMunicipalitySelected { get; set; }
 
+        private async Task OnMunicipalityChanged(ChangeEventArgs e)
+        {
+            var selected = e.Value?.ToString();
+
+            if (string.IsNullOrWhiteSpace(selected))
+            {
+                await OnMunicipalitySelected.InvokeAsync(null);
+            }
+            else
+            {
+                await OnMunicipalitySelected.InvokeAsync(selected);
+            }
+
+        }
     }
 }
