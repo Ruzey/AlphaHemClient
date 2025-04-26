ed25a3b35e98aa0e90b1540997e5f45732cf38fa
Christoffer Snobl <csnobl@hotmail.com>
2025-04-26 16:25:31 +0200

    Paginationkomponenten är färdig

---
+++ AlphaHemClient/Components/Pagination.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AlphaHemClient.Components
+{
+    // Author: Christoffer
+    public partial class Pagination
+    {
+        [Parameter]
+        public int CurrentPage { get; set; }
+        [Parameter]
+        public int TotalPages { get; set; }
+        [Parameter]
+        public EventCallback<int> OnPageSelected { get; set; }
+
+        private async Task SelectPage(int page)
+        {
+            if (page >= 1 && page <= TotalPages)
+            {
+                await OnPageSelected.InvokeAsync(page);
+            }
+        }
+    }
+}
