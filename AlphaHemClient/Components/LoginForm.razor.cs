3101d99127e8f54f1bda553c3a7397290192896d
Ruzey <mattias.grenholm@gmail.com>
2025-04-26 22:01:22 +0200

    Flyttat loginForm+registerForm till egna components

---
+++ AlphaHemClient/Components/LoginForm.razor.cs
@@ -0,0 +1,9 @@
+﻿namespace AlphaHemClient.Components
+{
+    //Author: Mattias
+    public partial class LoginForm
+    {
+
+        //Någon sorts auth service...
+    }
+}
--- AlphaHemClient/Components/RegisterForm.razor.cs
+++ AlphaHemClient/Components/RegisterForm.razor.cs
@@ -2,11 +2,10 @@
 using AlphaHemClient.Services;
 using Microsoft.AspNetCore.Components;
 
-
-namespace AlphaHemClient.Pages
+namespace AlphaHemClient.Components
 {
     //Author: Mattias
-    public partial class Login
+    public partial class RegisterForm
     {
         private List<AgencNamesViewModel> Agencies = new();
 
