38ea4e9a599d3f4b9d49b096e7e02463a464f1fe
Smilla Hallgren Larsson <smilla.hallgren-larsson1@yaelev.se>
2025-04-27 13:00:30 +0200

    name change

--- AlphaHemClient/Components/RegisterForm.razor.cs
+++ AlphaHemClient/Components/RegisterForm.razor.cs
@@ -7,7 +7,7 @@ namespace AlphaHemClient.Components
     //Author: Mattias
     public partial class RegisterForm
     {
-        private List<AgencNamesViewModel> Agencies = new();
+        private List<AgencyNamesViewModel> Agencies = new();
 
         [Inject]
         public AgencyService AgencyService { get; set; }
