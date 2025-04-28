fa69746551111d4c36eb4e95d4471c58023a3a59
Smilla Hallgren Larsson <smilla.hallgren-larsson1@yaelev.se>
2025-04-28 15:20:26 +0200

    liten fix

--- AlphaHemClient/Pages/RealtorProfilePage.razor.cs
+++ AlphaHemClient/Pages/RealtorProfilePage.razor.cs
@@ -18,7 +18,7 @@ namespace AlphaHemClient.Pages
         public int Id { get; set; }
 
         [Inject]
-        private RealtorService RealtorService { get; set; }  // Inject RealtorService
+        private RealtorService RealtorService { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
