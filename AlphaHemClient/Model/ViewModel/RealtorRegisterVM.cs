c09bc1f82268511fa0953ab230c1da5d205ebfaf
Dominika Simson <dominika.simson1@yaelev.se>
2025-05-13 14:49:04 +0200

    lite test med sorteringen

--- AlphaHemClient/Model/ViewModel/RealtorRegisterVM.cs
+++ AlphaHemClient/Model/ViewModel/RealtorRegisterVM.cs
@@ -5,7 +5,7 @@ namespace AlphaHemClient.Model.ViewModel
     //Author: ALL
     public class RealtorRegisterVM
     {
-        [Required, EmailAddress]
+        [Required, EmailAddress(ErrorMessage = "Epost är obligatoriskt.")]
         public string Email { get; set; }
         [Required]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{6,}$",
@@ -16,7 +16,7 @@ namespace AlphaHemClient.Model.ViewModel
         public string Password { get; set; }
         [Required, Compare("Password", ErrorMessage = "Dina lösenord matchar inte")]
         public string ConfirmPassword { get; set; }
-        [Required(ErrorMessage = "Namn är obligatoriskt.")]
+        [Required(ErrorMessage = "Förnamn är obligatoriskt.")]
         public string FirstName { get; set; }
         [Required(ErrorMessage = "Efternamn är obligatoriskt.")]
         public string LastName { get; set; }
