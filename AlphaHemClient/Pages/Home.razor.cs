d9f49b42287c13279a3539722580f0622a31a2cd
Conny Engström Wernersson <c.wernersson@gmail.com>
2025-05-15 08:57:20 +0200

    Fixed message when listings has been loaded.

--- AlphaHemClient/Pages/Home.razor.cs
+++ AlphaHemClient/Pages/Home.razor.cs
@@ -15,6 +15,7 @@ namespace AlphaHemClient.Pages
         private string? selectedMunicipality = null;
         private string? selectedCategory = null;
         private string? selectedSortOption = null;
+        private string? message = "Laddar...";
         private int currentPage = 1;
         private int totalPages = 1;
         private int pageSize = 9;
@@ -26,6 +27,15 @@ namespace AlphaHemClient.Pages
             municipalities = response.Data;
 
             await LoadListingsAsync(currentPage, pageSize);
+
+            if (viewModel.Listings.Count == 0)
+            {
+                message = "Inga bostäder hittades.";
+            }
+            else
+            {
+                message = null;
+            }
         }
 
         private async Task OnMunicipalityChanged(string? municipality)
